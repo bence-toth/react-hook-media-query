cf53f3e72a18e30943a7d644d3db80b914ce726d
Jan Kirchner <jan.k.kirchner@gmail.com>
2019-04-19 11:47:52 +0200

    move UpdateMatch inside useEffect to comply with eslint(react-hooks/exhaustive-deps)

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import {useState, useEffect} from 'react'
 const useMediaQuery = query => {
   const [match, setMatch] = useState(false)
 
-  const updateMatch = () => setMatch(window.matchMedia(query).matches)
-
   useEffect(() => {
+    const updateMatch = () => setMatch(window.matchMedia(query).matches)
+
     updateMatch()
     window.addEventListener('resize', updateMatch)
     window.addEventListener('orientationchange', updateMatch)
